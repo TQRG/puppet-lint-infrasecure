032feb1577045e44a92f3e6f5320a65ad881f86e
sofiaoreis <sofiareis1994@gmail.com>
2021-04-28 15:06:42 +0100

    hard coded secret rule update

--- lib/puppet-lint/plugins/check_hard_coded_secret.rb
+++ lib/puppet-lint/plugins/check_hard_coded_secret.rb
@@ -6,19 +6,22 @@ PuppetLint.new_check(:hardcode_secret) do
    NON_SECRETS = /gpg|path|type|buff|zone|mode|tag|header|scheme|length|guid/
 
    def check
+      # list of known credentials - not considered secrets by the community (https://puppet.com/docs/pe/2019.8/what_gets_installed_and_where.html#user_and_group_accounts_installed)
       user_default = ['pe-puppet', 'pe-webserver', 'pe-puppetdb', 'pe-postgres', 'pe-console-services', 'pe-orchestration-services','pe-ace-server', 'pe-bolt-server']
-      invalid_values = ['undefined', 'unset', 'www-data', 'wwwrun', 'www', 'no', 'yes', '[]']
+      # some were advised by puppet specialists
+      invalid_values = ['undefined', 'unset', 'www-data', 'wwwrun', 'www', 'no', 'yes', '[]', 'root']
       ftokens = filter_tokens(tokens)
       ftokens.each do |token|
          token_value = token.value.downcase
          token_type = token.type.to_s
-         if token_value =~ SECRETS and !(token_value =~ NON_SECRETS) and token_type == 'VARIABLE' and ["EQUALS", "FARROW"].include? token.next_code_token.type.to_s 
-            next_token = token.next_code_token
-            assign_side_type = next_token.next_code_token.type.to_s
-            assign_side_value = next_token.next_code_token.value.downcase
-            if ["STRING", "SSTRING"].include? assign_side_type and assign_side_type != 'VARIABLE' and assign_side_value.length > 3 and !invalid_values.include? assign_side_value and !(assign_side_value =~ /::|\/|\.|\\|\#/) and !user_default.include? assign_side_value
+         next_token = token.next_code_token
+         # accepts <VARIABLE> <EQUALS> secret OR <NAME> <FARROW> secret, checks if <VARIABLE> | <NAME> satisfy SECRETS but not satisfy NON_SECRETS 
+         if ["VARIABLE", "NAME"].include? token_type and ["EQUALS", "FARROW"].include? next_token.type.to_s and token_value =~ SECRETS and !(token_value =~ NON_SECRETS)
+            right_side_type = next_token.next_code_token.type.to_s
+            right_side_value = next_token.next_code_token.value.downcase
+            if ["STRING", "SSTRING"].include? right_side_type and right_side_value.length > 1 and !invalid_values.include? right_side_value and !(right_side_value =~ /::|\/|\.|\\/ ) and !user_default.include? right_side_value
                notify :warning, {
-                  message: "[SECURITY] Hard Coded Secret (line=#{token.line}, col=#{token.column}) | Do not keep secrets on your scripts as for $#{token_value} = #{assign_side_value} in #{token.line}. Use kms/heira/vault instead.",
+                  message: "[SECURITY] Hard Coded Secret (line=#{token.line}, col=#{token.column}) | Do not keep secrets on your scripts as for $#{token_value} = #{right_side_value} in #{token.line}. Use kms/heira/vault instead.",
                   line:    token.line,
                   column:  token.column,
                   token:   token_value
