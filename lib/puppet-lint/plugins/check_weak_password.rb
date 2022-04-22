cb5c5f0d5570b4295f75c5b5241d2ad12327447f
sofiaoreis <sofiareis1994@gmail.com>
2022-04-22 11:48:51 +0100

    refactoring for weak password message

--- lib/puppet-lint/plugins/check_weak_password.rb
+++ lib/puppet-lint/plugins/check_weak_password.rb
@@ -10,15 +10,17 @@ PuppetLint.new_check(:weak_password) do
             next if token.prev_code_token.nil? or token.next_code_token.nil?             
             if [:EQUALS, :FARROW].include? token_type and [:VARIABLE,:NAME].include? token.prev_code_token.type
                 left_side_value = token.prev_code_token.value.downcase
-                right_side_value = token.next_code_token.value.downcase
+                right_side_value = token.next_code_token.value
                 right_side_token = token.next_code_token
                 if left_side_value =~ Rules.password and checker.is_weak?(right_side_value) and right_side_value != '' and token.next_code_token.type == :SSTRING
+                    pwd_entropy = checker.calculate_entropy(right_side_value)
                     notify :warning, {
-                        message: "[SECURITY][CWE-521] Weak Password (line=#{right_side_token.line}, col=#{right_side_token.column}) | Passwords should be strong to be hard to uncover by hackers (weak_password=#{right_side_value}, entropy=#{checker.calculate_entropy(right_side_value)}). You should be using a password with at least 18 bits of entropy. In any case, secrets should be stored in services like kms/heira/vault not in plain text.",
+                        message: "[SECURITY][CWE-521] Weak Password (line=#{right_side_token.line}, col=#{right_side_token.column}) | Passwords should be strong to be hard to uncover by hackers (weak_password=#{right_side_value}, entropy=#{pwd_entropy}). Recommendation is to use a password with at least 18 bits of entropy. In any case, secrets should be stored in services like kms/heira/vault not in plain text.",
                         line:    right_side_token.line,
                         column:  right_side_token.column,
                         token:   right_side_value,
-                        cwe: 'CWE-521'
+                        cwe: 'CWE-521',
+                        entropy: pwd_entropy
                     }
                 end
              end
